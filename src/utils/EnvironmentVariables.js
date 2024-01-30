4d9d97f94293fdffb0e5991648cd05b32f45c2de
Vinicius Navarini <vinicius.navarini@compasso.com.br>
2024-01-29 21:06:33 -0300

    remove files

--- src/utils/EnvironmentVariables.js
+++
@@ -1,7 +0,0 @@
-const env = require('../config/env')
-
-module.exports = {
-  get_url (environment, route, query) {
-    return env.doo.urls[environment].baseUrl+`${env.doo.routes[route].path}`+ query
-  }
-}
